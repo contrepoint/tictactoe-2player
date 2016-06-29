cbf6cdd109e5335d223a554588235ca47442c950
Janice Shiu <contrepoint21@gmail.com>
2016-06-29 17:09:04 +0800

    User can see their number of games and the breakdown

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if user_signed_in?
+      @games_as_challenger = current_user.games_as_challenger
+      @games_as_challenged = current_user.games_as_challenged
+      @total_games = @games_as_challenger + @games_as_challenged
+    end
   end
 end
